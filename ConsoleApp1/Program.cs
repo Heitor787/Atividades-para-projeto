ac1fc6b379fa53ffd5bd97e7c73e221b54eeadc7
Heitor787 <gabrielheitorhg@gmail.com>
2025-04-30 08:55:49 -0300

    Ajustei a pasta errei o nome

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+
+        while (contador < 10)
+        {
+            Console.WriteLine("Esta é a frase número " + (contador + 1));
+            contador++;
+        }
+    }
+}
